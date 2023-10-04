7811dd964f2125e875f69e7ed2fe8f15b5788502
haroonth <kittisak.ma@logicspark.com>
2023-10-04 12:02:21 +0700

    ➕ add sqlite, passport, and jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.db',
+      entities: [User],
+      // entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    SharedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
